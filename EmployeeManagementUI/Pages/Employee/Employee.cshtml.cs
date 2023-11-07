b87b0a920abd8f4037d955db53c41ce597e23493
Mahabir <mahabirsingh.kis@gmail.com>
2023-11-07 21:25:20 +0530

    final commit

--- EmployeeManagementUI/Pages/Employee/Employee.cshtml.cs
+++ EmployeeManagementUI/Pages/Employee/Employee.cshtml.cs
@@ -1,4 +1,6 @@
+using EmployeeManagementUI.Factory;
 using EmployeeManagementUI.Models;
+using EmployeeManagementUIModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
@@ -7,19 +9,32 @@ namespace EmployeeManagementUI.Pages.Employee
 {
     public class EmployeeModel : PageModel
     {
-        public IList<EmployeesResponse> employeesResponseList { get; set; }
+        public IList<EmployeesResponseModel> employeesResponseList { get; set; }
+        [BindProperty]
+        public string SearchText { get; set; }
    
-
         public async Task OnGetAsync()
         {
-            using (var httpClient = new HttpClient())
+            await GetEmployees();
+        }
+
+        private async Task GetEmployees()
+        {
+            var response = await EmployeeManagementApiClientFactory.Instance.GetAllEmployee();
+            employeesResponseList = response.ToList();
+            if (!string.IsNullOrEmpty(SearchText))
             {
-                using (HttpResponseMessage response = await httpClient.GetAsync("https://localhost:44324/api/Employee/GetAll"))
-                {
-                    var apiResponse = await response.Content.ReadAsStringAsync();
-                    employeesResponseList = JsonConvert.DeserializeObject<List<EmployeesResponse>>(apiResponse);
-                }
+                employeesResponseList = employeesResponseList.Where(x =>
+                    x.Name?.ToLower().Contains(SearchText.ToLower()) == true ||
+                    x.Email?.ToLower().Contains(SearchText.ToLower()) == true
+                ).ToList();
             }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await GetEmployees();
+            return Page();
         }
     }
 }
